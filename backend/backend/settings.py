d9d12149b35726dee2bb106d2da9e6225503365a
mohamedwalid24636 <mewalid24636@gmail.com>
2026-05-10 16:29:41 +0300

    clean smtp gam54il456  config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -196,8 +196,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
@@ -205,7 +205,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 
 DEFAULT_FROM_EMAIL = os.getenv(
     'DEFAULT_FROM_EMAIL',
-    'Neurea Support <mewalid24636@gmail.com>'
+    'Neurea Support <Neureareply@neurea.local>'
 )
 
 
